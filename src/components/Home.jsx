ecaeaa505de186a389d010e88810763bf7b258e0
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2023-06-21 15:30:01 -0500

    add home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,39 @@
 import React from "react"
 import Navbar from "./Navbar"
 
+import Computer from "../assets/images/computer.png"
+
+import { FaPlayCircle } from "react-icons/fa"
+
+import "../assets/stylesheets/Home.css"
+
 const Home = () => {
     return (
-        <div>
-        <Navbar />
-        <h1>Home</h1>
-        
+
+        <div className="home">
+            <Navbar />
+            <div className="home-container">
+                <div className="home-content content">
+                    <h1>
+                        Cambia tu mentalidad y cambiaras tus<strong> Resultados</strong>
+                    </h1>
+                    <p>
+                        Que tus hijos no sean un excusa sino el impulso más grande para completar el programa SIMPLE TRADING
+                    </p>
+                    <div>
+                        <button>
+                            <FaPlayCircle className="play"/>
+                            VER  VIDEO
+                        </button>
+                        <button className="btn-active">
+                            INICIAR AHORA
+                        </button>
+                    </div>
+                </div>
+                <div className="home-content image">
+                    <img src={Computer} alt="computer" />
+                </div>
+            </div>
         </div>
     )
 }
